79a8fe72e0bee632600e9f78d668c80129d76bbb
KafeOvid <ayushmanng04@gmail.com>
2025-03-28 23:39:22 +0530

    Flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app =Flask(__name__)
+@app.route('/')
+def login():
+    return render_template('index.html')
+
+@app.route('/register')
+def about():
+    return '<h1>About</h1>'
+
+@app.route('/home')
+def home():
+    return '<h1>Home</h1>'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
